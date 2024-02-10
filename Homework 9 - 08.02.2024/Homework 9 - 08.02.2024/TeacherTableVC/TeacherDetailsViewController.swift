13b2aaddeb4c244f73fda67b61131408f8abde7c
Anzhelika Mihalchenia <anzhelichka1990@gmail.com>
2024-02-10 12:46:56 +0300

    creating relationships between students and teachers, creating details screens

---
+++ Homework 9 - 08.02.2024/Homework 9 - 08.02.2024/TeacherTableVC/TeacherDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TeacherDetailsViewController.swift
+//  Homework 9 - 08.02.2024
+//
+//  Created by Анжелика on 9.02.24.
+//
+
+import UIKit
+
+class TeacherDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
